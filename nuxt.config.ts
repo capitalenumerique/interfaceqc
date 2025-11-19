a400b817bdcca805aed64595dd826a3a8ddc06b4
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2025-11-18 23:20:44 -0500

    Cloudflare Workers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -77,7 +77,14 @@ export default defineNuxtConfig({
     future: {
         compatibilityVersion: 4,
     },
-    compatibilityDate: '2024-04-03',
+    compatibilityDate: '2024-09-19',
+    nitro: {
+        preset: 'cloudflare_module',
+        cloudflare: {
+            deployConfig: true,
+            nodeCompat: true,
+        },
+    },
     vite: {
         plugins: [
             ViteSvgLoader({
