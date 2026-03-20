17a0b6b05ca90086de5359dfd2338d4995ebdda6
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2026-03-20 02:54:13 -0400

    [FIX] Button hover

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -162,7 +162,7 @@ const onEscape = () => {
     background-color: var(--color-white);
     left: 16px;
     right: 16px;
-    box-shadow: 0 4px 32px 0 rgba(51, 50, 48, 0.05);
+    box-shadow: 0 4px 32px 0 rgba(51, 50, 48, 0.1);
     @media (--md) {
         top: 40px;
         left: auto;
@@ -234,7 +234,6 @@ const onEscape = () => {
 .primary-button.btn-cta {
     display: none;
     margin-left: auto;
-    box-shadow: 0 4px 32px 0 rgba(51, 50, 48, 0.05);
     @media (--md) {
         display: inline-flex;
     }
