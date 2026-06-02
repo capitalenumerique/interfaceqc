5c488fbfba84b720aad1ac41cd9f9bb75006e3ef
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2026-06-01 23:40:40 -0400

    [FEATURE] Ajuster les dates pour 2027

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -61,46 +61,12 @@ export default defineNuxtConfig({
     ],
     runtimeConfig: {
         public: {
-            start_date: '2026-05-26T00:00:00',
-            end_date: '2026-05-28T00:00:00',
+            start_date: '2027-06-01T00:00:00',
+            end_date: '2027-06-03T00:00:00',
             ticketing_url: 'https://ti.to/cnum/interface2027',
             commitTag: process.env.COMMIT_TAG || process.env.NUXT_ENV_CURRENT_GIT_TAG,
             commitShortSha: process.env.COMMIT_SHORT_SHA || process.env.NUXT_ENV_CURRENT_GIT_SHA,
             swapcardGraphqlEndpoint: process.env.NUXT_PUBLIC_SWAPCARD_GRAPHQL_ENDPOINT,
-            milestones: {
-                PRE_SALE: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-                CALL_FOR_SPEAKER: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-                CALL_FOR_VOLUNTEERS: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-                TICKET_SALE: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-                EVENT: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-                THANK_YOU: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-                APP_AVAILABLE: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-                REPLAY_AVAILABLE: {
-                    start_date: '2026-05-26T00:00:00',
-                    end_date: '2026-05-28T00:00:00',
-                },
-            },
         },
         swapcardToken: process.env.NUXT_SWAPCARD_AUTH_TOKEN,
         swapcardCommunityId: process.env.NUXT_SWAPCARD_COMMUNITY_ID,
